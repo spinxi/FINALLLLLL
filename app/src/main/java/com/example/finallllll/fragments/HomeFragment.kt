d6adfe4492027e16660ac096ca3f4d34e3716a9a
spinxi <ernorevevoo@gmail.com>
2022-01-12 13:53:04 -0500

    Initial commitს ს ტესტ ტეს ტესტ

---
+++ app/src/main/java/com/example/finallllll/fragments/HomeFragment.kt
@@ -0,0 +1,11 @@
+package com.example.finallllll.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.finallllll.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+
+}
---
+++ app/src/main/java/com/example/finallllll/fragments/UserFragment.kt
@@ -0,0 +1,7 @@
+package com.example.finallllll.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.finallllll.R
+
+class UserFragment : Fragment(R.layout.fragment_user) {
+}
